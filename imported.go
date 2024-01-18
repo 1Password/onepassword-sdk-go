4e47d2adfd9ed4db5834daf807e8e7f315914810
AndyTitu <andi.titu@agilebits.com>
2024-01-18 13:09:58 +0200

    Fix the RNG logic

--- imported.go
+++ imported.go
@@ -3,7 +3,7 @@ package onepassword
 import (
 	"context"
 	"crypto/rand"
-
+	"fmt"
 	extism "github.com/extism/go-sdk"
 	"github.com/tetratelabs/wazero/api"
 )
@@ -13,20 +13,21 @@ func ImportedFunctions() []extism.HostFunction {
 	return []extism.HostFunction{randomFillFunc()}
 }
 
-// randomFillFunc returns an Extism Function that writes random bytes into the WASM core's memory using crypto/rand.
+// randomFillFunc returns an Extism Function that writes n (input) random bytes into memory using crypto/rand, and returns their offset.
 func randomFillFunc() extism.HostFunction {
 	randomFill := extism.NewHostFunctionWithStack("random_fill_imported", func(ctx context.Context, p *extism.CurrentPlugin, stack []uint64) {
-		ptr := api.DecodeU32(stack[0])
-		length := api.DecodeU32(stack[1])
+		length := api.DecodeU32(stack[0])
 
 		b := make([]byte, length)
 		_, err := rand.Read(b)
 		if err != nil {
 			panic(err)
 		}
-
-		p.Memory().Write(ptr, b)
-	}, []api.ValueType{api.ValueTypeI32, api.ValueTypeI32}, []api.ValueType{})
+		stack[0], err = p.WriteBytes(b)
+		if err != nil {
+			panic(fmt.Errorf("failed to write bytes: %v", err))
+		}
+	}, []api.ValueType{api.ValueTypeI32}, []api.ValueType{api.ValueTypeI64})
 	randomFill.SetNamespace("op-random")
 
 	return randomFill
