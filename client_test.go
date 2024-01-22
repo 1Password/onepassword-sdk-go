76e56035fd3b0864d4c9fafe64f424288b0406e8
AndyTitu <andi.titu@agilebits.com>
2024-01-22 16:48:15 +0100

    Move release client test in client_test

--- client_test.go
+++ client_test.go
@@ -1,100 +1,95 @@
 package onepassword
 
 import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNoToken(t *testing.T) {
-	clientFactory := NewTestClientFactory()
+func preTest() {
+	sharedCore = NewTestCore()
+}
 
+func TestNoToken(t *testing.T) {
+	preTest()
 	// missing token
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithIntegrationInfo(DefaultIntegrationName, DefaultIntegrationVersion))
 	assert.Equal(t, "cannot create a client without specifying a Service Account Token", err.Error())
 }
 
 func TestNoIntegrationName(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo("", DefaultIntegrationVersion))
 	assert.Equal(t, "cannot create a client without defining an app name and version. If you don't want to specify any, use the provided constants: 'DefaultIntegrationName', 'DefaultIntegrationVersion'", err.Error())
 }
 
 func TestInvalidIntegrationNameLength(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo("12345678901234567890123456789012345678901234567890", DefaultIntegrationVersion))
 	assert.Equal(t, "integration name can't be longer than 40 characters", err.Error())
 }
 
 func TestInvalidIntegrationNameCharacters(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo("$", DefaultIntegrationVersion))
 	assert.Equal(t, "integration name can only contain digits, letters and allowed symbols", err.Error())
 }
 
 func TestNoIntegrationVersion(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo(DefaultIntegrationName, ""))
 	assert.Equal(t, "cannot create a client without defining an app name and version. If you don't want to specify any, use the provided constants: 'DefaultIntegrationName', 'DefaultIntegrationVersion'", err.Error())
 }
 
 func TestInvalidIntegrationVersionLength(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo(DefaultIntegrationName, "12345678901234567890123456789012345678901234567890"))
 	assert.Equal(t, "integration version can't be longer than 20 characters", err.Error())
 }
 
 func TestInvalidIntegrationVersionCharacters(t *testing.T) {
+	preTest()
 	token := "my_token"
 
-	clientFactory := NewTestClientFactory()
-
-	_, err := clientFactory.NewClient(
+	_, err := NewClient(context.TODO(),
 		WithServiceAccountToken(token),
 		WithIntegrationInfo(DefaultIntegrationName, "$"))
 	assert.Equal(t, "integration version can only contain digits, letters and allowed symbols", err.Error())
 }
 
-func NewTestClientFactory() *ClientFactory {
-	return &ClientFactory{core: TestCore{}}
+func TestClientIsReleasedWhenGarbageCollected(t *testing.T) {
+	preTest()
+	testCore := NewTestCore()
+	sharedCore = testCore
+	_, err := NewClient(context.TODO(),
+		WithServiceAccountToken("test_token"),
+		WithIntegrationInfo(DefaultIntegrationName, DefaultIntegrationVersion))
+	require.NoError(t, err)
+	assert.True(t, testCore.clientExists[0])
+	runtime.GC()
+	assert.False(t, testCore.clientExists[0])
 }
-
-type TestCore struct{}
-
-func (c TestCore) InitClient(config ClientConfig) (*uint64, error) {
-	var id uint64
-	return &id, nil
-}
-
-func (c TestCore) Invoke(invokeConfig Invocation) (*string, error) {
-	response := "secret"
-	return &response, nil
-}
-
-func (c TestCore) ReleaseClient(clientID uint64) {}
