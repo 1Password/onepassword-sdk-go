7535a0777ade5c437de5579d4759f806bab46212
AndyTitu <andi.titu@agilebits.com>
2024-01-23 17:08:46 +0100

    Add Go user facing code

---
+++ example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	onepassword "github.com/1password/1password-go-sdk"
+)
+
+// This is an example of how to retrieve a secret from 1Password and set it as SECRET_ENV_VAR using the SDK client.
+
+func main() {
+	token := os.Getenv("OP_SERVICE_ACCOUNT_TOKEN")
+
+	client, err := onepassword.NewClient(context.TODO(),
+		onepassword.WithServiceAccountToken(token),
+		onepassword.WithIntegrationInfo(onepassword.DefaultIntegrationName, onepassword.DefaultIntegrationVersion),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	secret, err := client.Secrets.Resolve("op://xw33qlvug6moegr3wkk5zkenoa/bckakdku7bgbnyxvqbkpehifki/foobar")
+	if err != nil {
+		panic(err)
+	}
+
+	doSomethingSecret(*secret)
+}
+
+func doSomethingSecret(secret string) {
+	err := os.Setenv("SECRET_ENV_VAR", secret)
+	if err != nil {
+		panic(err)
+	}
+}
