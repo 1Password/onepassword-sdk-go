f5d99c3ff17fa0d12843efbbdab98264b8611dfb
AndyTitu <andi.titu@agilebits.com>
2024-01-31 13:52:51 +0100

    Add mutex to shared core to make parallel calls tests pass

--- internal/shared_core.go
+++ internal/shared_core.go
@@ -5,6 +5,7 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	extism "github.com/extism/go-sdk"
 )
@@ -39,18 +40,19 @@ func ReleaseCore() {
 
 // SharedCore implements Core in such a way that all created client instances share the same core resources.
 type SharedCore struct {
+	mu     sync.Mutex
 	plugin *extism.Plugin
 }
 
 // InitClient creates a client instance in the current core module and returns its unique ID.
-func (c SharedCore) InitClient(config ClientConfig) (*uint64, error) {
+func (c *SharedCore) InitClient(config ClientConfig) (*uint64, error) {
 	marshaledConfig, err := json.Marshal(config)
 	if err != nil {
 		return nil, err
 	}
 
 	// first return parameter is a sys.Exit code, which we don't need since the error is fully recoverable
-	_, res, err := c.plugin.Call(initClientFuncName, marshaledConfig)
+	res, err := c.call(initClientFuncName, marshaledConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -63,12 +65,12 @@ func (c SharedCore) InitClient(config ClientConfig) (*uint64, error) {
 }
 
 // Invoke calls specified business logic from core
-func (c SharedCore) Invoke(invokeConfig InvokeConfig) (*string, error) {
+func (c *SharedCore) Invoke(invokeConfig InvokeConfig) (*string, error) {
 	input, err := json.Marshal(invokeConfig)
 	if err != nil {
 		return nil, err
 	}
-	_, res, err := c.plugin.Call(invokeFuncName, input)
+	res, err := c.call(invokeFuncName, input)
 	if err != nil {
 		return nil, err
 	}
@@ -79,15 +81,25 @@ func (c SharedCore) Invoke(invokeConfig InvokeConfig) (*string, error) {
 }
 
 // ReleaseClient releases memory in the core associated with the given client ID.
-func (c SharedCore) ReleaseClient(clientID uint64) {
+func (c *SharedCore) ReleaseClient(clientID uint64) {
 	marshaledClientID, err := json.Marshal(clientID)
 	if err != nil {
 		c.plugin.Log(extism.LogLevelWarn, fmt.Sprintf("memory couldn't be released: %s", err.Error()))
 	}
-	_, _, err = c.plugin.Call(releaseClientFuncName, marshaledClientID)
+	_, err = c.call(releaseClientFuncName, marshaledClientID)
 	if err != nil {
 		c.plugin.Log(extism.LogLevelWarn, "memory couldn't be released")
 	}
+}
+
+func (c *SharedCore) call(functionName string, serializedParameters []byte) ([]byte, error) {
+	c.mu.Lock()
+	_, response, err := c.plugin.Call(functionName, serializedParameters)
+	c.mu.Unlock()
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 // `loadWASM` returns the WASM core loaded into an `extism.Plugin`.
