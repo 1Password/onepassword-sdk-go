29fcfb1e32f37b7fa40892b13f13b7b64d2c03d8
Andi Titu <45081667+AndyTitu@users.noreply.github.com>
2024-03-11 15:08:51 +0100

    Apply suggestions from code review
    Use Lucy's suggestions
    
    Co-authored-by: Lucy Butcher <89952129+libutcher@users.noreply.github.com>

--- secrets.go
+++ secrets.go
@@ -4,8 +4,8 @@ import "github.com/1password/onepassword-sdk-go/internal"
 
 // SecretsAPI represents all operations the SDK client can perform on 1Password secrets.
 type SecretsAPI interface {
-	// Resolve returns the secret the provided reference points to.
-	// Secret references are url-like strings that link to secret fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
+	// Resolve returns the secret the provided secret reference points to.
+	// Secret references are url-like strings that point to fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
 	// Read more about secret references: https://developer.1password.com/docs/cli/secret-references
 	Resolve(reference string) (string, error)
 }
@@ -19,7 +19,7 @@ func NewSecretsSource(inner InnerClient) *SecretsSource {
 	return &SecretsSource{inner}
 }
 
-// Resolve returns the secret the provided reference points to.
+// Resolve returns the secret the provided secret reference points to.
 // Secret reference syntax: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
 // Read more about secret references: https://developer.1password.com/docs/cli/secret-references
 func (s SecretsSource) Resolve(reference string) (string, error) {
