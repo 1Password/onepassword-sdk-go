b418a54bb89a3422542d59cbafeeaba2a5463b37
AndyTitu <andi.titu@agilebits.com>
2024-03-20 12:25:35 +0100

    Shorten the secret references code comment

--- secrets.go
+++ secrets.go
@@ -5,7 +5,7 @@ import "github.com/1password/onepassword-sdk-go/internal"
 // SecretsAPI represents all operations the SDK client can perform on 1Password secrets.
 type SecretsAPI interface {
 	// Resolve returns the secret the provided secret reference points to.
-	// Secret references are url-like strings that point to fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
+	// Secret references point to fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
 	// Read more about secret references: https://developer.1password.com/docs/cli/secret-references
 	Resolve(reference string) (string, error)
 }
