8d13a2fa9475a2c3c4436faa6923a2673c67be4d
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-04-05 17:49:28 +0000

    Update core to version bb563bac

--- secrets.go
+++ secrets.go
@@ -8,15 +8,10 @@ import (
 	"github.com/1password/onepassword-sdk-go/internal"
 )
 
-// SecretsAPI represents all operations the SDK client can perform on 1Password secrets.
 type SecretsAPI interface {
-	// Resolve returns the secret the provided secret reference points to.
-	// Secret references point to fields in 1Password. They have the following format: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
-	// Read more about secret references: https://developer.1password.com/docs/cli/secret-references
 	Resolve(ctx context.Context, secretReference string) (string, error)
 }
 
-// SecretsSource implements SecretsAPI relying on an inner client for operations with secrets.
 type SecretsSource struct {
 	internal.InnerClient
 }
@@ -25,9 +20,6 @@ func NewSecretsSource(inner internal.InnerClient) *SecretsSource {
 	return &SecretsSource{inner}
 }
 
-// Resolve returns the secret the provided secret reference points to.
-// Secret reference syntax: op://<vault-name>/<item-name>[/<section-name>]/<field-name>
-// Read more about secret references: https://developer.1password.com/docs/cli/secret-references
 func (s SecretsSource) Resolve(ctx context.Context, secretReference string) (string, error) {
 
 	resultString, err := clientInvoke(ctx, s.InnerClient, "Resolve", map[string]interface{}{
