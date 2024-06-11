077663452a409ecd0c91d2678eba00e20131c814
Omar Miraj <omar.miraj@agilebits.com>
2024-06-11 10:30:41 -0400

    Change release back to 1Password GO SDK

--- scripts/release.sh
+++ scripts/release.sh
@@ -92,4 +92,4 @@ fi
 # Login with Github CLI
 gh auth login --with-token <<< ${GITHUB_CLI_TOKEN} 
 
-gh release create "${version_number}" --title "Release ${version_number}" --notes "${changelog_content}" --repo github.com/MOmarMiraj/onepassword-sdk-go
+gh release create "${version_number}" --title "Release ${version_number}" --notes "${changelog_content}" --repo github.com/1Password/onepassword-sdk-go
