eb28aefeb73bdef410f206bdacde498ce7938ff4
Omar Miraj <omar.miraj@agilebits.com>
2025-03-04 06:44:35 -0500

    fix import example

--- example/main.go
+++ example/main.go
@@ -10,11 +10,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/1password/onepassword-sdk-go"
 )
 
 // [developer-docs.sdk.go.sdk-import]-start
-
+import "github.com/1password/onepassword-sdk-go"
 // [developer-docs.sdk.go.sdk-import]-end
 
 func main() {
