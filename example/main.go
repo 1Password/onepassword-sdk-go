b10a35bd4fdacea9d44d3adfe9acf30bb37a6b0a
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-06-10 16:10:49 -0500

    Revert back sdk import

--- example/main.go
+++ example/main.go
@@ -8,12 +8,10 @@ import (
 	"encoding/pem"
 	"fmt"
 	"os"
-
-	"github.com/1password/onepassword-sdk-go"
 )
 
 // [developer-docs.sdk.go.sdk-import]-start
-
+import "github.com/1password/onepassword-sdk-go"
 // [developer-docs.sdk.go.sdk-import]-end
 
 func main() {
