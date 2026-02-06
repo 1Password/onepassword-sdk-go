f6c38d8385080a13542dabdd3261f700243b1fee
Morgan Ward <morgan.ward@agilebits.com>
2026-02-06 10:26:12 -0500

    updated examples to include latest functionality for stable release

--- example/service_account/main.go
+++ example/service_account/main.go
@@ -8,10 +8,11 @@ import (
 	"encoding/pem"
 	"fmt"
 	"os"
+
+	"github.com/1password/onepassword-sdk-go"
 )
 
 // [developer-docs.sdk.go.sdk-import]-start
-import "github.com/1password/onepassword-sdk-go"
 
 // [developer-docs.sdk.go.sdk-import]-end
 
@@ -31,12 +32,20 @@ func main() {
 	}
 	// [developer-docs.sdk.go.client-initialization]-end
 
+	groupID := os.Getenv("OP_GROUP_ID")
+	if groupID == "" {
+		panic("OP_GROUP_ID is required")
+	}
+
 	item := createAndGetItem(client)
 	createSSHKeyItem(client)
 	createAndReplaceDocumentItem(client)
 	createAndAttachAndDeleteFileFieldItem(client)
 	getAndUpdateItem(client, item.VaultID, item.ID)
 	listVaultsAndItems(client, item.VaultID)
+	showcaseVaultOperations(client)
+	showcaseBatchItemOperations(client, item.VaultID)
+	showcaseGroupPermissionOperations(client, item.VaultID, groupID)
 	generatePasswords()
 	resolveSecretReference(client, item.VaultID, item.ID, "username")
 	resolveBulkSecretReferences(client, item.VaultID, item.ID, "username", "password")
@@ -45,6 +54,211 @@ func main() {
 	fmt.Println(sharelink)
 	archiveItem(client, item.VaultID, item.ID)
 	deleteItem(client, item.VaultID, item.ID)
+}
+
+func showcaseVaultOperations(client *onepassword.Client) {
+
+	// [developer-docs.sdk.go.create-vault]-start
+	description := "This vault was created with the Go SDK."
+	// Create a vault with a description
+	create_params := onepassword.VaultCreateParams{
+		Title:       "Go SDK Vault",
+		Description: &description,
+	}
+
+	created_vault, err := client.Vaults().Create(context.Background(), create_params)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created vault with description: %v\n", created_vault)
+	// [developer-docs.sdk.go.create-vault]-start
+
+	// [developer-docs.sdk.go.get-vault-overview]-start
+	// Get vault overview
+	vaultOverview, err := client.Vaults().GetOverview(context.Background(), created_vault.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Vault overview: %v\n", vaultOverview)
+	// [developer-docs.sdk.go.get-vault-overview]-end
+
+	// [developer-docs.sdk.go.get-vault-details]-start
+	// Get vault details
+	vault, err := client.Vaults().Get(context.Background(), vaultOverview.ID, onepassword.VaultGetParams{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Vault details: %v\n", vault)
+	// [developer-docs.sdk.go.get-vault-details]-end
+
+	// [developer-docs.sdk.go.update-vault]-start
+	updateParams := onepassword.VaultUpdateParams{
+		Title:       nil,
+		Description: nil,
+	}
+
+	name := "Go SDK Updated Vault"
+	description = "Updated description from Go SDK"
+	updateParams.Title = &name
+	updateParams.Description = &description
+
+	// Update the vault
+	updated_vault, err := client.Vaults().Update(context.Background(), created_vault.ID, updateParams)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Updated Vault: ", updated_vault.Title)
+	// [developer-docs.sdk.go.update-vault]-end
+
+	// [developer-docs.sdk.go.delete-vault]-start
+	// Delete vault
+	client.Vaults().Delete(context.Background(), created_vault.ID)
+	// [developer-docs.sdk.go.delete-vault]-end
+
+	// [developer-docs.sdk.go.list-vaults]-start
+	// List vaults
+	vaults, err := client.Vaults().List(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	for _, vault := range vaults {
+		fmt.Println("VAULT ID: ", vault.ID, "VAULT NAME: ", vault.Title)
+	}
+	// [developer-docs.sdk.go.list-vaults]-end
+}
+
+func showcaseBatchItemOperations(client *onepassword.Client, vaultID string) {
+	// [developer-docs.sdk.go.batch-create-items]-start
+	sectionID := "extraDetails"
+	var itemsToCreate []onepassword.ItemCreateParams
+	for i := 1; i <= 3; i++ {
+		itemsToCreate = append(itemsToCreate, onepassword.ItemCreateParams{
+			Title:    fmt.Sprintf("Login %d created with the SDK", i),
+			Category: onepassword.ItemCategoryLogin,
+			VaultID:  vaultID,
+			Fields: []onepassword.ItemField{
+				{
+					ID:        "username",
+					Title:     "username",
+					Value:     "Wendy_Appleseed",
+					FieldType: onepassword.ItemFieldTypeText,
+				},
+				{
+					ID:        "password",
+					Title:     "password",
+					Value:     "my_weak_password123",
+					FieldType: onepassword.ItemFieldTypeConcealed,
+				},
+				{
+					ID:        "onetimepassword",
+					Title:     "one-time password",
+					Value:     "otpauth://totp/my-example-otp?secret=jncrjgbdjnrncbjsr&issuer=1Password",
+					SectionID: &sectionID,
+					FieldType: onepassword.ItemFieldTypeTOTP,
+				},
+			},
+			Sections: []onepassword.ItemSection{
+				{
+					ID:    sectionID,
+					Title: "Extra Details",
+				},
+			},
+			Tags: []string{"test tag1", "test tag 2"},
+			Websites: []onepassword.Website{
+				{
+					URL:              "1password.com",
+					AutofillBehavior: onepassword.AutofillBehaviorAnywhereOnWebsite,
+					Label:            "my custom website",
+				},
+			},
+		})
+	}
+
+	// Create all items in the same vault in a single batch
+	batchCreateResponse, err := client.Items().CreateAll(context.Background(), vaultID, itemsToCreate)
+	if err != nil {
+		panic(err)
+	}
+
+	var itemIDs []string
+	for _, res := range batchCreateResponse.IndividualResponses {
+		if res.Content != nil {
+			fmt.Printf("Created Item %q (%s)\n", res.Content.Title, res.Content.ID)
+			itemIDs = append(itemIDs, res.Content.ID)
+		} else if res.Error != nil {
+			fmt.Printf("[Batch create] Something went wrong: %s\n", res.Error)
+		}
+	}
+	// [developer-docs.sdk.go.batch-create-items]-end
+
+	// [developer-docs.sdk.go.batch-get-items]-start
+	// Get multiple items form the same vault in a single batch
+	batchGetResponse, err := client.Items().GetAll(context.Background(), vaultID, itemIDs)
+	if err != nil {
+		panic(err)
+	}
+	for _, res := range batchGetResponse.IndividualResponses {
+		if res.Content != nil {
+			fmt.Printf("Obtained Item %q (%s)\n", res.Content.Title, res.Content.ID)
+		} else if res.Error != nil {
+			fmt.Printf("[Batch get] Something went wrong: %s\n", res.Error)
+		}
+	}
+	// [developer-docs.sdk.go.batch-get-items]-end
+
+	// [developer-docs.sdk.go.batch-delete-items]-start
+	// Delete multiple items from the same vault in a single batch
+	batchDeleteResponse, err := client.Items().DeleteAll(context.Background(), vaultID, itemIDs)
+	if err != nil {
+		panic(err)
+	}
+	for id, res := range batchDeleteResponse.IndividualResponses {
+		if res.Error != nil {
+			fmt.Printf("[Batch delete] Something went wrong: %s\n", res.Error)
+		} else {
+			fmt.Printf("Deleted item %s\n", id)
+		}
+	}
+	// [developer-docs.sdk.go.batch-delete-items]-end
+}
+
+func showcaseGroupPermissionOperations(client *onepassword.Client, vaultID string, groupID string) {
+	// Grant group permissions to a vault.
+	groupAccess := onepassword.GroupAccess{
+		GroupID:     groupID,
+		Permissions: onepassword.ReadItems,
+	}
+	err := client.Vaults().GrantGroupPermissions(context.Background(), vaultID, []onepassword.GroupAccess{groupAccess})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Granted group permissions to vault.")
+
+	// update group permissions for vaults.
+	groupVaultAccess := onepassword.GroupVaultAccess{
+		GroupID:     groupID,
+		VaultID:     vaultID,
+		Permissions: onepassword.ReadItems | onepassword.CreateItems | onepassword.UpdateItems,
+	}
+	err = client.Vaults().UpdateGroupPermissions(context.Background(), []onepassword.GroupVaultAccess{groupVaultAccess})
+	if err != nil {
+		panic(err)
+	}
+
+	// Revoke group permissions from a vault.
+	err = client.Vaults().RevokeGroupPermissions(context.Background(), vaultID, groupID)
+	if err != nil {
+		panic(err)
+	}
+
+	// [developer-docs.sdk.go.get-group]-start
+	// Get a group
+	group, err := client.Groups().Get(context.Background(), groupID, onepassword.GroupGetParams{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Group details: %v\n", group)
+	// [developer-docs.sdk.go.get-group]-end
 }
 
 func listVaultsAndItems(client *onepassword.Client, vaultID string) {
