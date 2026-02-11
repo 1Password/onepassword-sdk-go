fa9c78f3300e825d1bb90a269b8228c5d51d4f57
Morgan Ward <morgan.ward@agilebits.com>
2026-02-11 08:33:51 -0500

    updated lint ignore comment for group operations

--- example/service_account/main.go
+++ example/service_account/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 // [developer-docs.sdk.go.sdk-import]-start
-import 	"github.com/1password/onepassword-sdk-go"
+import "github.com/1password/onepassword-sdk-go"
 // [developer-docs.sdk.go.sdk-import]-end
 
 func main() {
@@ -766,7 +766,7 @@ func showcaseVaultOperations(client *onepassword.Client) {
 	// [developer-docs.sdk.go.list-vaults]-end
 }
 
-//lint:ignore U1000 NOTE: this is just to showcase how to do vault crudl operations
+//lint:ignore U1000 NOTE: this is just to showcase how to do group permissions operations
 func showcaseGroupPermissionOperations(client *onepassword.Client, vaultID string, groupID string) {
 	// [developer-docs.sdk.go.grant-group-permissions]-start
 	// Grant group permissions to a vault.
