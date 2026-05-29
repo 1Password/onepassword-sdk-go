677b6c6555ebf37a7074d0cf2fae9ed180311861
Till! <till@users.noreply.github.com>
2026-05-29 21:00:49 +0200

    fix: cgo handling in the code base (#263)
    * fix: cgo handling in the code base
    
    add missing build tag to shared_lib_core_unix (to match its
    companion file).
    
    fix the client_builder_no_cgo to return a runtime error, instead
    of using an undefined symbol to produce a compile error. the
    problem (IMHO, of course) is that this will not let me "build"
    otherwise.
    
    resolves: #260
    
    * chore: style (errors vs fmt)

--- client_builder_no_cgo.go
+++ client_builder_no_cgo.go
@@ -2,8 +2,11 @@
 
 package onepassword
 
+import "errors"
+
 // WithDesktopAppIntegration specifies a client should use the desktop app to authenticate. Set to your 1Password account name as shown at the top left sidebar of the app, or your account UUID.
 func WithDesktopAppIntegration(accountName string) ClientOption {
-	var _ = ERROR_WithDesktopAppIntegration_requires_CGO_To_Cross_Compile_See_README_CGO_Section
-	return nil
+	return func(c *Client) error {
+		return errors.New("WithDesktopAppIntegration requires CGO to cross-compile. See README CGO section")
+	}
 }
