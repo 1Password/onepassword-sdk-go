1bc929bc3c48c1ae7ce91e892912c01a95d7a488
Till Klampaeckel <till@php.net>
2026-05-29 17:59:30 +0200

    chore: style (errors vs fmt)

--- client_builder_no_cgo.go
+++ client_builder_no_cgo.go
@@ -2,11 +2,11 @@
 
 package onepassword
 
-import "fmt"
+import "errors"
 
 // WithDesktopAppIntegration specifies a client should use the desktop app to authenticate. Set to your 1Password account name as shown at the top left sidebar of the app, or your account UUID.
 func WithDesktopAppIntegration(accountName string) ClientOption {
 	return func(c *Client) error {
-		return fmt.Errorf("WithDesktopAppIntegration requires CGO to cross-compile. See README CGO section")
+		return errors.New("WithDesktopAppIntegration requires CGO to cross-compile. See README CGO section")
 	}
 }
